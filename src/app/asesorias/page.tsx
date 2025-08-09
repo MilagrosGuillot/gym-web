2c933222a28b5348904e49a4349dde8145c8df35
MilagrosGuillot <miliguillot12@gmail.com>
2025-08-09 14:53:00 -0300

    seccion asesorias

--- src/app/asesorias/page.tsx
+++ src/app/asesorias/page.tsx
@@ -1,7 +1,26 @@
 export default function AsesoriasPage() {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-black">
-        <h1 className="text-3xl md:text-5xl font-bold text-red-600">Vista de Asesorías</h1>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black p-6">
+      <div className="flex flex-col gap-3 bg-red-900  border border-red-500 shadow-[0_0_0_1px_red] text-white rounded-lg shadow-lg max-w-3xl p-8 [&>p]:text-[15px] md:[&>p]:text-[24px]">
+      <h1 className="text-xl md:text-3xl font-bold italic mb-4">
+
+          ¿QUÉ SON LAS ASESORÍAS 1 A 1?
+        </h1>
+
+        <p className="mb-4">
+          Si estás buscando llevar tu alimentación a otro nivel, las asesorías 1 a 1 son para vos.
+        </p>
+        <p className="mb-4">
+        Según la asesoría que elijas, tendrás un plan de nutrición y/o entrenamiento 100% personalizado, suplementación adaptada, información extra, chequeos, soporte directo y actualizaciones cuando lo necesites.
+        </p>
+        <p className="mb-4">
+        Voy a resolver todas tus dudas, enseñarte la técnica correcta, motivarte y ajustar tu plan para que sigas progresando.
+        </p>
+        <p className="mb-4">
+        Y, sobre todo, voy a asegurarme de que tengas una rutina y alimentación que disfrutes, que puedas mantener y que te lleven a tus resultados sin frustraciones.
+        </p>
       </div>
-    );
-  }
+      </div>
+
+  );
+}
